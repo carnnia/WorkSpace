ddb987a3f82c4411744c759586934200bddd16ab
Casey Carnnia <carnnia@gmail.com>
2012-11-29 18:18:58 -0500

    starting frm scratch

---
+++ HelloWorld/src/hangManDemo.java
@@ -0,0 +1,19 @@
+/*
+Name:  Casey Carnnia
+Date:  012.13.2012
+Scope: Write a program to test your class definition: HangMan
+*/
+
+public class hangManDemo
+{
+	//- declare the main method
+	public static void main (String[] args)
+	{
+	HangMan myGame = new HangMan();
+	
+	System.out.println("let's bigin the game  ");
+	myGame.newGame();
+	
+	 
+	}//main END
+}//class END
